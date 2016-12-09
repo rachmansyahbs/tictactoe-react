b25c9b31517e4efe4ed9fb93646eec72be12b7eb
rachmansyahbs <rachmansyahbs@gmail.com>
2016-12-09 20:45:03 +0800

    More component separation

--- src/Components/TicTacToe.js
+++ src/Components/TicTacToe.js
@@ -6,37 +6,7 @@ import {EMPTY, PLAYER_1, PLAYER_1_SIGN, PLAYER_2, PLAYER_2_SIGN, BOARD_SIZE} fro
 
 import TTTBoard from './TTTBoard';
 
-class TTTPlayerLabel extends React.Component {
-  render() {
-    return (
-      <span>
-      {this.props.player_name} ({this.props.player_sign})
-      </span>
-    );
-  }
-}
-
-class TTTScore extends React.Component {
-  render() {
-    return (
-      <div>
-        <div><TTTPlayerLabel player_name={this.props.player_name} player_sign={this.props.player_sign} /></div>
-        <div>{this.props.score}</div>
-      </div>
-    )
-  }
-}
-
-class TTTScoreBoard extends React.Component {
-  render() {
-    return (
-      <div>
-        <TTTScore player_name={this.props.players[0].name} player_sign={this.props.players[0].sign} score={this.props.players[0].score} />
-        <TTTScore player_name={this.props.players[1].name} player_sign={this.props.players[1].sign} score={this.props.players[1].score} />
-      </div>
-    );
-  }
-}
+import {TTTPlayerLabel, TTTScore, TTTScoreBoard} from './TTTScoreBoard';
 
 export default class TicTacToe extends React.Component {
   constructor(props) {
