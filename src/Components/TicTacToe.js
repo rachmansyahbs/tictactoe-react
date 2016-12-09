026992376450086146fc64be4ab809d3321a0ff1
rachmansyahbs <rachmansyahbs@gmail.com>
2016-12-09 20:11:07 +0800

    Update TicTacToe component with win condition checking

--- src/Components/TicTacToe.js
+++ src/Components/TicTacToe.js
@@ -2,17 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import utils from '../scripts/utils';
-
-// Constant for game states
-const EMPTY = 0;
-const PLAYER_1 = 1;
-const PLAYER_2 = 2;
-const BOARD_SIZE = 3;
-const game_state = {
-  current_player: PLAYER_1,
-  board: utils.createMatrix(3, EMPTY),
-  scores: [0, 0]
-}
+import {EMPTY, PLAYER_1, PLAYER_1_SIGN, PLAYER_2, PLAYER_2_SIGN, BOARD_SIZE} from '../data/game_constants';
 
 // DEBUG
 // inline style
@@ -36,24 +26,33 @@ const cell_style = {
 }
 // 
 
+class TTTPlayerLabel extends React.Component {
+  render() {
+    return (
+      <span>
+      {this.props.player_name} ({this.props.player_sign})
+      </span>
+    );
+  }
+}
+
 class TTTScore extends React.Component {
   render() {
     return (
       <div>
-        <div>{this.props.player_name}</div>
+        <div><TTTPlayerLabel player_name={this.props.player_name} player_sign={this.props.player_sign} /></div>
         <div>{this.props.score}</div>
       </div>
     )
   }
 }
 
-class TTTStateBoard extends React.Component {
+class TTTScoreBoard extends React.Component {
   render() {
     return (
       <div>
-        <TTTScore player_name={this.props.players[0].name} score={this.props.players[0].score} />
-        <TTTScore player_name={this.props.players[1].name} score={this.props.players[1].score} />
-        <div>{this.props.players[this.props.current_player_index].name}'s Turn</div>
+        <TTTScore player_name={this.props.players[0].name} player_sign={this.props.players[0].sign} score={this.props.players[0].score} />
+        <TTTScore player_name={this.props.players[1].name} player_sign={this.props.players[1].sign} score={this.props.players[1].score} />
       </div>
     );
   }
@@ -101,18 +100,52 @@ class TTTRow extends React.Component {
 export default class TicTacToe extends React.Component {
   constructor(props) {
     super(props);
-    this.state = Object.assign({}, game_state);
+    this.state = {
+      current_player: PLAYER_1,
+      scores: [0, 0],
+      board: utils.createMatrix(BOARD_SIZE, EMPTY)
+    }
   }
 
   getFlattenedBoard() {
-    return [].concat.apply([], this.state.board);
+    return utils.flattenMatrix(this.state.board);
   }
 
   checkWinner() {
-    // Check win conditions
-    // Rows
-    // Cols
-    // Diagonals
+    var winner = false;
+    var winning_sets = [];
+
+    var flattened_board = this.getFlattenedBoard();
+
+    // Define win conditions
+    var possible_winning_sets = [];
+    // - Rows
+    possible_winning_sets = possible_winning_sets.concat(utils.getRowsIndexes(BOARD_SIZE));
+    // - Cols
+    possible_winning_sets = possible_winning_sets.concat(utils.getColsIndexes(BOARD_SIZE));
+    // - Diagonals
+    possible_winning_sets = possible_winning_sets.concat(utils.getDiagonalsIndexes(BOARD_SIZE));
+
+    // Generic function to check win conditions
+    for(let i = 0; i < possible_winning_sets.length; i++) {
+      let set = utils.getArrayElements(flattened_board, possible_winning_sets[i]);
+      let check = utils.checkArraySameValues(set);
+      if(check.result && check.value != EMPTY) {
+        winner = check.value;
+        winning_sets.push(set)
+        // - Alternatively can be modified for early exit/return if does not need to handle multiple winning sets (possible, but unlikely)
+        // return (winner: check.value, sets: [set]);
+      }
+    }
+
+    if(winning_sets.length) {
+      var result = {
+        winner: winner,
+        sets: winning_sets
+      }
+
+      return result;
+    }
 
     return false;
   }
@@ -127,13 +160,16 @@ export default class TicTacToe extends React.Component {
   }
 
   startNewGame() {
+    // Intentionally not resetting the player's turn
+    // Player that made the last move in the previous game (whether it ends up in draw or win for the player) will move second in this game
     this.setState({
-      board: game_state.board
+      board: utils.createMatrix(BOARD_SIZE, EMPTY)
     })
   }
 
   resetGameScores() {
     this.setState({
+      current_player: PLAYER_1,
       scores: [0, 0]
     });
     this.startNewGame();
@@ -168,34 +204,49 @@ export default class TicTacToe extends React.Component {
     // Check winner
     var winner = this.checkWinner();
     if(winner) {
-      // TO-DO
+      this.addScore(winner.winner);
     }
 
     // Update state
     this.setState(new_state);
   }
 
-  render() {
-    var is_game_finished = this.isGameFinished();
-    var winner_index = this.checkWinner();
-    var winner_text = winner_index ? "Player " + winner_index : "Draw";
+  addScore(player) {
+    var scores = this.state.scores;
+    scores[player-1]++;
+    this.setState({scores: scores});
+  }
 
-    var players = [
-      {name: "Player 1", score: this.state.scores[0]},
-      {name: "Player 2", score: this.state.scores[1]},
-    ]
+  getPlayers() {
+    return [
+      {name: "Player 1", sign: PLAYER_1_SIGN, score: this.state.scores[0]},
+      {name: "Player 2", sign: PLAYER_2_SIGN, score: this.state.scores[1]},
+    ];
+  }
+
+  render() {
+    var players = this.getPlayers();
+    var is_game_finished = this.isGameFinished();
+    var check_winner = this.checkWinner();
+    var winner_index = false;
+    if(check_winner) {
+      winner_index = check_winner.winner;
+    }
+    var winner_text = winner_index ? ("Winner: Player " + winner_index) : "Draw";
+    var current_player_text = (
+      <TTTPlayerLabel player_name={players[this.state.current_player-1].name} player_sign={players[this.state.current_player-1].sign} />
+    );
 
     return (
       <div>
-        <TTTStateBoard players={players} current_player_index={this.state.current_player - 1}/>
+        <TTTScoreBoard players={players} />
         <div>
-          {is_game_finished && 
-            <div>Finished</div>
-          }
-          {is_game_finished && 
-            <div>{winner_text}</div>
-          }
+          {is_game_finished ? 
+            winner_text : 
+            <div>{current_player_text}'s Turn</div>
+          } 
         </div>
+
         <div className="board" style={board_style}>
           {this.state.board.map((row, row_i) => (
           <TTTRow key={"row" + (row_i + 1)}
@@ -203,6 +254,13 @@ export default class TicTacToe extends React.Component {
           />
           ))}
         </div>
+
+        {is_game_finished &&
+        <div>
+          <button onClick={this.startNewGame.bind(this)}>Play Again</button>
+          <button onClick={this.resetGameScores.bind(this)}>Reset Scores</button>
+        </div>
+        }
       </div>
     );
   }
