585e03a1a919ce99b8efad81927dcbe4f231eac1
rachmansyahbs <rachmansyahbs@gmail.com>
2016-12-09 17:54:15 +0800

    Use tictactoe gameboard component in main app module

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import TicTacToe from './Components/TicTacToe';
+
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        
+        <TicTacToe />
       </div>
     );
   }
